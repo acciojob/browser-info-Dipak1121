abaf4ff71c735ee65cc0ce435edc20dbc99b787e
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-07 22:03:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", findHistory);
+
+function findHistory(){
+    let browserName = navigator.appName;
+    let browserVersion = navigator.appVersion;
+
+    let message = `You are using ${browserName} ${browserVersion} version`;
+    document.getElementById("browser-info").innerText = message;
+}
